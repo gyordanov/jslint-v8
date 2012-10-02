c340ae4546927d41466ed1e345206d9306676b6c
Galin Yordanov <gyordanov@gmail.com>
2012-10-02 15:31:02 -0400

    updates ..

--- v8shell.js
+++ v8shell.js
@@ -23,6 +23,51 @@ var outputErrors = function (errors) {
     return output.join('');
 };
 
-if (!JSHINT(read(input_filename), {})) {
+var options = {
+      bitwise: false,
+      curly: true,
+      eqeqeq: true,
+      immed:true,
+      latedef: true,
+      noarg: true,
+      plusplus:true,
+      regexp:true,
+      undef: true,
+      unused: true,
+      trailing: true,
+      maxparams: 5,
+      maxdepth: 5,
+      maxstatements: 25,
+      maxcomplexity: 10,
+
+      evil: true,
+
+      laxcomma: true,
+
+      browser: true,
+      node: true,
+      devel: true,
+      es5: true,
+      jquery: true,
+
+      globalstrict: false,
+      strict: false,
+      boss: false
+
+};
+var globals = {
+      roundNumber: true,
+      roundNumberStr: true,
+      _: true,
+      JST: true,
+      read: true,
+      jQuery:true,
+      Correlate: true,
+      Corr: true,
+      Backbone: true,
+      JSHINT: true
+};
+
+if (!JSHINT(read(input_filename), options, globals)) {
     print(outputErrors(JSHINT.errors));
 }
